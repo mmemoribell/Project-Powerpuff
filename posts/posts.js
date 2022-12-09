8422ea6d2ed81d457d7fe6251d4aa70d645da608
mmemoribell <mmemoriibell@gmail.com>
2022-12-09 11:53:36 -0500

    mhm

--- posts/posts.js
+++ posts/posts.js
@@ -1,70 +1,66 @@
-  "use strict";
-  /* Posts Page JavaScript */
+ "use strict"; function getLoginData () { return JSON.parse(window.localStorage.getItem("login-data")) || {}; } 
+ // this is the cards where the post will show up 
+ et postsDiv = document.querySelector("#posts"); 
+ // log in auth 
+ const loginData = getLoginData(); 
+ const options = { headers: { Authorization: `Bearer ${loginData.token}`, } } 
+ // api calling username 
+ function postUser() { fetch( api + "/api/users/", options) 
+ .then(response => response.json()) 
+ .then(data => 
+  { let postData = data.username; }); } 
+  const username = postUser(); 
+  function postId() { 
+    fetch( api + "/api/posts/", options) 
+    .then(response => response.json()) 
+    .then(data => { 
+      let postIdNum = data.postId; }); } 
+      const postNum = postId(); 
+      function postTextM() { 
+        fetch( api + "/api/posts/", options) 
+        .then(response => response.json()) 
+        .then(data => { 
+          let postW = data.text; 
+          console.log(postW); }); 
+        } 
+      const postText = postTextM(); 
+      // Function to get all users via fetch() 
+      function getAllUsers () {
+         // GET /api/users 
+         const loginData = getLoginData(); 
+         const options = { method: "GET", headers: { Authorization: `Bearer ${loginData.token}`, },};
+          // note: the api variable is defined in auth.js 
+          fetch(api + "/api/users", options) 
+          .then(response => response.json()) 
+          .then(users => { 
 
-  const $q = (s) => document.querySelector(s);
-  const cardHolder = document.getElementsByClassName("#cards");
-  const usernamePro = $q("#card-title");
-  const postText = $q("#card-apply");
+            const posts = fetch(api + "/api/posts", options)
+  .then(response => response.json())
+  .then(post => post);
+  posts.then(post => console.table(post))
+console.log(posts)
+ // Promise {<pending>}
+            // Do something with the users array... 
+            console.log(users); 
+          }); } function createCard(post) { 
+            const cardDiv = document.createElement("div"); 
+            cardDiv.classList.add("card");
+            cardDiv.style.width = "18rem"; 
+            const cardBody = document.createElement("div"); 
+            cardDiv.classList.add("card-body");  
+            const heading5 = document.createElement("h5"); 
+            cardDiv.classList.add("card-title"); 
+            heading5.innerText = postNum; 
+            const heading6 = document.createElement("h6"); 
+            cardDiv.classList.add("card-subtitle"); 
+            heading6.innerText = username; 
+            const paragraph = document.createElement("p"); 
+            cardDiv.classList.add("card-text"); 
+            paragraph.innerText = postText; 
+            cardDiv.appendChild(cardBody); 
+            cardBody.append(heading5, heading6, paragraph); return cardBody; } 
 
-
-  function getLoginData() {
-  return JSON.parse(window.localStorage.getItem("login-data")) || {};
-}
-  const loginData = getLoginData();
-
-  function displayProfilePost() {
-  const options = {
-    method: "GET",
-    headers: {
-      // This header is how we authenticate our user with the
-      // server for any API requests which require the user
-      // to be logged-in in order to have access.
-      // In the API docs, these endpoints display a lock icon.
-      Authorization: `Bearer ${loginData.token}`,
-    },
-  };
-
-  fetch(api + "/api/posts/", options)
-    .then((response) => response.json())
-    .then((posts) => {
-      posts.forEach((post) => {
-        buildPostCard(cardHolder, post);
-
-      });
-    });
-}
-
-function cardDisplayData(data) {
-  const cardTitle = usernamePro;
-  const postPassage = postText;
-  cardTitle.innerText = data.username;
-  postPassage.innerText = data.text;
-  cardTitle.innerText = `@${data.username}`
-
- 
-  fetch("https://microbloglite.herokuapp.com/api/posts", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-      Authorization: `Bearer ${loginData.token}`,
-    },
-    body: JSON.stringify(bodyData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      window.location.href = "./posts.html";
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-function loadName() {
-  const loginData = getLoginData();
-  bioUserName.innerText = `${loginData.username}`;
-}
-  
-window.onload = () => {
-  displayProfilePost();
-  logoutButton.onclick = logout;
-}
+ window.onload = () => { 
+  for (const post of posts) { 
+    const card = createCard(post); 
+    postsDiv.appendChild(card); } };
